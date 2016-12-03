89529f45af816c6117a2840d4d963e0970c43303
Zaidock <zaidock@users-MacBook-Pro.local>
2016-12-03 14:52:32 +1100

    Gang Sim V0.0.0.2
    *Added Day Cycle
    *Added Hotel Room
    *Added Collisions to hotel building
    *Bugs

---
+++ Gang Simulator/Assets/Scripts/Collisions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Collisions : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider){
+		Debug.Log ("ENTERING Hotel");
+		SceneManager.LoadScene ("Hotel");
+	}
+}
